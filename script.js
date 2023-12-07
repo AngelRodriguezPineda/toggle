d088dafb36ab0d4b1d89976858c21e7e6ebe960c
Angel Rodriguez Pineda <angel@twochange.ong>
2023-12-06 18:37:42 -0600

    first commit

---
+++ script.js
@@ -0,0 +1,7 @@
+let toggleBtn = document.querySelector('.toggleBtn');
+let conteiner = document.querySelector('.conteiner');
+toggleBtn.onclick = function () {
+    conteiner.classList.toggle('active');
+}
+
+console.log()
